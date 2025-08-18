72b9a0a739e8c8264a3b877d41cd7e8707f86416
Lookei-Lin <drakepso2@gmail.com>
2025-08-18 16:05:02 +0200

    Add files via upload

---
+++ JavaScripts/calc_index.js
@@ -0,0 +1,21 @@
+// Calculator 
+
+const display = document.getElementsByClassName('display')[0];
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value ="Error";
+    }
+}
+    
